1eb0a6d2c8f1a5ce7785331624097150b9251654
Douglas <douglas@toolsbr.com>
2017-12-26 15:32:00 -0200

    Aula 48

--- src/fundamentos/operadores/Destructuring.kt
+++ src/fundamentos/operadores/Destructuring.kt
@@ -1,2 +1,16 @@
 package fundamentos.operadores
 
+data class Carro(val marca: String, val modelo: String)
+
+
+fun main(args: Array<String>) {
+    val (a, b) = Carro("Ford", "Fusion")
+    println("$a $b")
+
+    val (marido, mulher) = listOf("João", "Maria")
+    println("$marido e $mulher")
+
+    val (_, _, terceiroLugar) = listOf("Kimi", "Hamilton", "Alosno")
+    println("$terceiroLugar terminou em terceiro lugar.")
+
+}
--- src/fundamentos/operadores/Relacionais.kt
+++ src/fundamentos/operadores/Relacionais.kt
@@ -1,2 +1,17 @@
 package fundamentos.operadores
 
+import java.util.*
+
+fun main(args: Array<String>) {
+    println("Banana" === "Banana")
+
+    val d1 = Date(0)
+    val d2 = Date(0)
+
+    //Igualdade referencial
+    println("Resultado com '===' ${d1 === d2}")
+
+    //Igualdade estrutural
+    println("Resultado com '==' ${d1 == d2}")
+    //println("Resultado com '==' ${d1.equals(d2)}")
+}
