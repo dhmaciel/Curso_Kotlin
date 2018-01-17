b4c210f6a0042c1e43178c83c8fc74e97f2cb2a8
Douglas <douglas@toolsbr.com>
2018-01-17 14:59:12 -0200

    Aula 88 Labmdas - Filter

--- src/lambdas/Filter.kt
+++ src/lambdas/Filter.kt
@@ -1,2 +1,19 @@
 package lambdas
 
+data class Aluno(val nome: String, val nota: Double)
+
+fun main(args: Array<String>) {
+    val alunos = arrayListOf(
+            Aluno("Pedro", 7.4),
+            Aluno("Maria", 8.0),
+            Aluno("Sara", 4.3),
+            Aluno("Sebastião", 4.1),
+            Aluno("Alice", 9.7)
+    )
+
+    val aprovados = alunos.filter { it.nota >= 7.0 }.sortedBy { it.nome }
+    val reprovados = alunos.filter { it.nota < 7.0 }.sortedBy { it.nome }
+
+    println("Alunos aprovados: $aprovados")
+    println("Alunos reprovados: $reprovados")
+}
--- src/lambdas/SortedBy.kt
+++ src/lambdas/SortedBy.kt
@@ -1,2 +1,12 @@
 package lambdas
 
+fun main(args: Array<String>) {
+    val nomes = arrayListOf("Renata", "Bernardo", "Willian", "Andreia", "Caio")
+    val ordenadosAsc = nomes.sortedBy { it }
+    val ordenadosDesc = nomes.sortedByDescending { it }
+
+    println("Ordenação crescente: $ordenadosAsc")
+    println("Ordenação decrescente: $ordenadosDesc")
+
+
+}
